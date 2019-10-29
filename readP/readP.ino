a4dc121897883a2a23669e22805bb8aed793cfa3
Jeong Hye-mi <hyami222@gmail.com>
2019-10-30 01:17:39 +0900

    Add files via upload

--- readP/readP.ino
+++ readP/readP.ino
@@ -1,75 +1,56 @@
 
-#include <ros.h>
 #include "AD7705.h"
-#include "jaco_msgs/Gripper.h"
 
-#define intervalR 10
-
-const int ss = 41;
-const int ss2 = 45;
+const int ss = 48;
+const int ss2 = 49;
 const double vref = 5.0;
 
 double v;
 double v1;
-unsigned long range_timer;
 
-ros::NodeHandle nh;
-jaco_msgs::Gripper gripper;
-ros::Publisher chatter("gripperJnt", &gripper);
 
 AD7705 ad7705_1(ss,vref);
-//AD7705 ad7705_2(ss2,vref);
+AD7705 ad7705_2(ss2,vref);
 
 void setup() {
-  
-  nh.initNode();
-  nh.advertise(chatter);
-  
-//  Serial.begin(9600);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
   SPI.begin();
-  
+//     pinMode(ss, OUTPUT);
+//
+//    digitalWrite(ss, HIGH);
+
+
   ad7705_1.reset();
   ad7705_1.init(AD7705::CHN_AIN1);
   ad7705_1.init(AD7705::CHN_AIN2);
 
-//  ad7705_2.reset();
-//  ad7705_2.init(AD7705::CHN_AIN1);
-//  ad7705_2.init(AD7705::CHN_AIN2);
-  
-  
-//  SPI.begin();
+  ad7705_2.reset();
+  ad7705_2.init(AD7705::CHN_AIN1);
+  ad7705_2.init(AD7705::CHN_AIN2);
   
 }
 
 void loop() {
-
-  unsigned long currentMillis = millis();
-
-  if (currentMillis >= range_timer + intervalR){
-
-    range_timer = currentMillis + intervalR;
-
-    v = ad7705_1.readValue(AD7705::CHN_AIN1);
-    gripper.jnt1 = v;
-  //  Serial.print(" 1 ");
-  //  Serial.print(v);
-
-  //  v = ad7705_1.readADResult(AD7705::CHN_AIN2);
+  // put your main code here, to run repeatedly:
   
-    v = ad7705_1.readValue(AD7705::CHN_AIN2);
-    gripper.jnt2 = v;
-    //  Serial.print(" : ");
-  //  Serial.println(v);
+  v = ad7705_1.readValue(AD7705::CHN_AIN1);
+  
+  Serial.print(" 1 ");
+  Serial.print(v);
 
-    chatter.publish(&gripper);
-  }
+    
+  v1 = ad7705_1.readValue(AD7705::CHN_AIN2);
   
-  nh.spinOnce();
-  
+  Serial.print(" : ");
+  Serial.println(v1);
+  delay(500);
 //
-//  v1 = ad7705_2.readValue(AD7705::CHN_AIN1);
-//  Serial.print(" 2 ");
-//  Serial.print(v1);
+  
+  v = ad7705_2.readValue(AD7705::CHN_AIN2);
+//  
+  Serial.print(" 2 ");
+  Serial.println(v);
 //
 ////  v = ad7705_1.readADResult(AD7705::CHN_AIN2);
 //  
